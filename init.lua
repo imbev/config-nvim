8d7a3bb6eb4dbdfd6cfe2f2c643b05a89726cb1e
Isaac Beverly <imbev@protonmail.com>
2025-05-11 16:03:18 -0500

    Add keybind to format buffer

--- init.lua
+++ init.lua
@@ -76,6 +76,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
     vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
     vim.keymap.set('n', '<Space>e', '<cmd>lua vim.diagnostic.setqflist()<cr>', opts)
+    vim.keymap.set('n', '<Space>f', '<cmd>lua vim.lsp.buf.format()<cr>', opts)
   end,
 })
 
